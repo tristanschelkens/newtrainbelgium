9420890cb90d42efd35a1cf4eae928be42053a38
Tristan_YaTi <tristanschelkens@gmail.com>
2026-03-06 09:30:17 +0100

    s

--- js/loadComponents.js
+++ js/loadComponents.js
@@ -7,5 +7,5 @@ function loadComponent(id, file) {
     .catch((err) => console.error("Component load error:", file, err));
 }
 
-loadComponent("navbar", "../components/navbar.html");
+loadComponent("navbar", "./components/navbar.html");
 loadComponent("footer", "../components/footer.html");
