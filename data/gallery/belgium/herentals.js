00dcaca1754c487b37691f426cfb7c2e9af99a37
tristanschelkens <tristanschelkens@gmail.com>
2026-05-22 22:53:56 +0200

    s

--- data/gallery/belgium/herentals.js
+++ data/gallery/belgium/herentals.js
@@ -10,8 +10,24 @@ station({
     {
       id: "photo-0",
       operator: "NMBS/SNCB",
-      alt: "Leuven HLE 1913 + 6x M7 ",
-      numbers: "1913",
+      alt: "Herentals HLE 1701 + 4x I11",
+      numbers: "1701",
+      consist: [traction("HLE17 01"), carriage("4x I11", true)],
+      images: ["Herentals_HLE1701I11.webp"],
+    },
+            {
+      id: "photo-1",
+      operator: "NMBS/SNCB",
+      alt: "Herentals 10x M7",
+      numbers: "76010",
+      consist: [traction("M7")],
+      images: ["Herentals_M7.webp","Herentals_M7_2"],
+    },
+        {
+      id: "photo-2",
+      operator: "NMBS/SNCB",
+      alt: "Herentals HLE 1902 + 8x M6",
+      numbers: "1902",
       consist: [traction("HLE19 02"), carriage("8x M6", true)],
       images: ["Herentals_HLE1902M6.webp"],
     }
