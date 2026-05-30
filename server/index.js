9c682e648f05fdf15af755cf0c65e243d5532e99
Tristan_YaTi <tristanschelkens@gmail.com>
2026-05-30 01:24:15 +0100

    Increase JSON body limit for photo upload

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ if (!dbUrl) {
 
 const pool = new Pool({ connectionString: dbUrl || undefined });
 
-app.use(express.json({ limit: '1mb' }));
+app.use(express.json({ limit: '15mb' }));
 app.use(cookieParser(sessionSecret));
 
 function normalizeUsername(value) {
